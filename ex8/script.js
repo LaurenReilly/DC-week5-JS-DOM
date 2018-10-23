8ddefdd2606d565ad7b87a0a139901edddccaeed
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-10-23 19:19:39 -0400

    added some comments on other strategies I could have used

--- ex8/script.js
+++ ex8/script.js
@@ -3,7 +3,7 @@
     0) Make it so that after entering the task and clicking the "Add Task" button, that new task appears inside the todo-list.
     1) Add the ability to check off tasks by clicking on them - (text-decoration:strikethrough is a nice way to)
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
-    SUPER BONUS) Make each task editable
+    SUPER BONUS) Make each task editable FOR THIS SUPER BONUS: could have made an edit button that would have popped up a prompt and then you could have set the li equal to the prompted text. OR could have set an attribute on the li of "content-editable" and that allows you to edit the text
 */
 
 var button = document.getElementById("add-button");
@@ -37,9 +37,13 @@ function deleteItem(e) {
 }
 
 list.addEventListener("click", function(e) {
+    //if they click on delete button delete list item
     if (e.target.classList.contains("delete")) {
         deleteItem(e);
     } else {
+        //if they just click on li then do line-thru
+        //could also have done this when defining the original li tag and add another function inside the button event listener
+        //that is listening on the li and uses this.style
         e.target.style.textDecoration = "line-through";
     }
 });
